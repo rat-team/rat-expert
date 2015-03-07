96cde59641af2fcf517897ca14b880b972fe0706
Сергей <seregachern@mail.ru>
2015-03-08 01:40:34 +0300

    server: attribute-redact-page

--- ExpertSystem/redact/system.py
+++ ExpertSystem/redact/system.py
@@ -3,14 +3,10 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from ExpertSystem.models import System, Attribute, AttributeValue
+from ExpertSystem.models import System
 from ExpertSystem.utils import sessions
 from ExpertSystem.utils.decorators import require_post_params
-from ExpertSystem.utils.decorators import require_creation_session
 from scripts.recreate import recreate
-
-
-#TODO везде проверки на существование
 
 
 def create_db(request):
@@ -38,25 +34,6 @@ def add_system(request, **kwargs):
     return render(request, "add_system/add_system.html")
 
 
-@require_creation_session()
-def add_attributes(request):
-    session = request.session.get(sessions.SESSION_ES_CREATE_KEY)
-    system = System.objects.get(id=session["system_id"])
-    all_attributes = Attribute.objects.filter(system=system)
-
-    attributes = []
-    for attribute in all_attributes:
-        attr_values = AttributeValue.objects.filter(attr=attribute)
-        values = []
-        for value in attr_values:
-            values.append(value.value)
-        attributes.append({
-            "name": attribute.name,
-            "values": values,
-        })
-
-    return render(request, "add_system/add_attributes.html", {"attributes": attributes})
-
 
 @require_http_methods(["POST"])
 @require_post_params("system_name")
@@ -76,24 +53,4 @@ def insert_system(request):
 
     system = System.objects.create(name=system_name)
     sessions.init_es_create_session(request, system.id)
-    return HttpResponse(json.dumps(response), content_type="application/json")
-
-
-@require_http_methods(["POST"])
-@require_post_params("form_data")
-@require_creation_session()
-def insert_attributes(request):
-    session = request.session.get(sessions.SESSION_ES_CREATE_KEY)
-    system = System.objects.get(id=session["system_id"])
-    Attribute.objects.filter(system=system).delete()
-    form_data = json.loads(request.POST.get("form_data"))
-    for attr_json in form_data:
-        attribute = Attribute.objects.create(name=attr_json["name"], system=system)
-        for val in attr_json["values"]:
-            AttributeValue.objects.create(system=system, attr=attribute, value=val)
-
-    response = {
-        "code": 0,
-    }
-
     return HttpResponse(json.dumps(response), content_type="application/json")
