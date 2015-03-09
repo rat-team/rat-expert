0d71f1fbe291880464ba12d41028e84b92cb14df
Сергей <seregachern@mail.ru>
2015-03-09 11:49:59 +0300

    redact objects

--- ES/urls.py
+++ ES/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 
 from django.contrib import admin
 from ExpertSystem.redact.attributes import add_attributes, insert_attributes, delete_attribute_value, delete_attribute
+from ExpertSystem.redact.objects import add_objects, insert_objects
 from ExpertSystem.redact.parameters import add_parameters, insert_parameters, delete_parameter
 from ExpertSystem.redact.system import create_db, add_system, insert_system
 from ExpertSystem.views import index, creators, answer, reset
@@ -28,10 +29,14 @@ urlpatterns = patterns('',
     url(r'^add_system/$', add_system, name="add_system"),
     url(r'^add_attributes$', add_attributes, name="add_attributes"),
     url(r'^add_parameters$', add_parameters, name="add_parameters"),
+    url(r'^add_objects$', add_objects, name="add_objects"),
 
     url(r'^insert_system/$', insert_system, name="insert_system"),
     url(r'^insert_attributes/$', insert_attributes, name="insert_attributes"),
     url(r'^insert_parameters/$', insert_parameters, name="insert_parameters"),
+    url(r'^insert_objects/$', insert_objects, name="insert_objects"),
+
+    # url(r'^insert_objects/$', insert_objects, name="insert_objects"),
     url(r'^delete_attribute_value/$', delete_attribute_value, name="delete_attribute_value"),
     url(r'^delete_attribute/$', delete_attribute, name="delete_attribute"),
     url(r'^delete_parameter/$', delete_parameter, name="delete_parameter")
---
+++ ExpertSystem/static/js/add_objects.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+    setupAjaxDjango();
+
+    //Удаляем селекторы с AttributeValue
+    var delete_attribute_value_btn = $('.delete_select_object_attribute_value');
+    delete_attribute_value_btn.click(deleteAttributeValue);
+    function deleteAttributeValue(event){
+        var selector = $(event.target).closest('.select__object_new_attribute_value');
+        selector.remove();
+    }
+
+
+    //Добавляем селектор
+    var add_attribute_value_btn = $('.object__add_attribute_value');
+    add_attribute_value_btn.click(addAttributeValue);
+    function addAttributeValue(event){
+        $block = $('.object__attribute_value_template').eq(0);
+        $block = $block.children().first();
+        $blockClone = $block.clone();
+        $blockClone.find('.delete_select_object_attribute_value').on('click', deleteAttributeValue);
+        var $target = $(event.target).closest('.btn');
+        $blockClone.insertBefore($target);
+    }
+
+
+    //Удаляем объект
+    var delete_object_btn = $('.delete_object');
+    delete_object_btn.click(deleteObject);
+    function deleteObject(event){
+        var $target = $(event.target).closest('.btn');
+        $target.closest('.system_object').remove();
+    }
+
+
+    //Добавляем новый объект
+    var add_new_object_btn = $('.add_new_object');
+    add_new_object_btn.click(addNewObject);
+    function addNewObject(event){
+        objects = $('.objects');
+        $block = $('.object_template').eq(0).children().first();
+        $blockClone = $block.clone();
+        $blockClone.removeClass('system_object_template').addClass('system_object');
+        $blockClone.find('.delete_object').click(deleteObject);
+        $blockClone.find('.object__add_attribute_value').click(addAttributeValue);
+        $blockClone.find('.delete_select_object_attribute_value').click(deleteAttributeValue);
+
+        objects.append($blockClone);
+    }
+
+
+    //Делаем снимок и инсертим в базу
+    var add_objects_form = $('#add_objects_form');
+    add_objects_form.submit(function(){
+        var objectItems = $('.system_object');
+		var objects = [];
+		_.each(objectItems, function(item) {
+			var objectJSON = {
+				id: $(item).find('input[name=id]').val(),
+				name: $(item).find('input[name=name]').val(),
+				attribute_values: []
+			};
+
+            var object_attributes_values = $(item).find('.select__object_new_attribute_value');
+            _.each(object_attributes_values, function(attributeValue){
+                var id = $(attributeValue).find('select[name=object__attribute_value]').val();
+                objectJSON.attribute_values.push(id);
+            });
+			//_.each($(item).find('input[name=value]'), function(attrValueInput) {
+			//	attrJSON.values.push($(attrValueInput).val());
+			//});
+			objects.push(objectJSON);
+		});
+
+        $('#add_objects_form').find('input[name=form_data]').val(JSON.stringify(objects));
+        var formdata = new FormData($(this)[0]);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formdata,
+            processData: false,
+            contentType: false,
+            success: function(data){
+                if (data["code"] == 0) {
+                    toastr.success('Объекты обновлены', 'Успех!');
+                }else{
+                    toastr.error(data["msg"]);
+                }
+            },
+            error: function(msg){
+                toastr.error('Что-то пошло не так, попоробуйте отправить заново', 'Ошибка!');
+            }
+        });
+
+        return false;
+    });
+
+});
