71766beb72c43deb7dfae4f68101764120477b01
Сергей <seregachern@mail.ru>
2015-03-14 20:08:01 +0300

    [*] settings_local

--- ES/settings_local.py
+++ ES/settings_local.py
@@ -1,21 +1,7 @@
-"""
-Django settings for ES project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
-PROJECT_NAME = "es"
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 _PATH = os.path.abspath(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -24,13 +10,11 @@ _PATH = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = 'dpp#zd_v1hqcy*fm7lxc3xush6uvij1m*7l#4ydxeobv_b^3!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
-DEPLOY_ROOT = os.environ.get("rat_expert_root", _PATH)
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -90,6 +74,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_URL = '/static/'
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
     os.path.join(BASE_DIR,  'templates/add_system'),
@@ -106,71 +92,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 QUERIES_DEBUG = False
 
-LOG_FILE = os.path.join(DEPLOY_ROOT, "log",  PROJECT_NAME + ".log")
-
-MEDIA_ROOT = os.path.join(DEPLOY_ROOT, "media")
+MEDIA_ROOT = os.path.join(_PATH, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(DEPLOY_ROOT, "static")
-STATIC_URL = '/static/'
-
-
-if not os.path.exists(LOG_FILE):
-    try:
-        os.makedirs(os.path.dirname(LOG_FILE))
-        open(LOG_FILE, 'a').close()
-    except OSError as e:
-        LOG_FILE = "/dev/null"
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-    },
-    'handlers': {
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-        },
-        'logfile': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': LOG_FILE,
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'standard',
-        },
-        'console':{
-            'level':'INFO',
-            'class':'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['console'],
-            'propagate': True,
-            'level':'WARN',
-        },
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'ExpertSystem': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-        },
-    }
-}
-
-
-
-try:
-    from settings_local import *
-except ImportError:
-    pass
