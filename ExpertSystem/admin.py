0fc83b2c654b104765226016c72111d6e05d8383
Сергей <seregachern@mail.ru>
2015-03-14 20:35:10 +0300

    admin.py

--- ExpertSystem/admin.py
+++ ExpertSystem/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
 
+admin.site.register(System)
+admin.site.register(Attribute)
+admin.site.register(Parameter)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(AttributeValue)
+admin.site.register(SysObject)
+admin.site.register(Rule)
