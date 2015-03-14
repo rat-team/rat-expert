da78744fb5ae15e5460d25fb97868827e12dace6
i.abashin <abashinos@gmail.com>
2015-03-15 01:22:14 +0300

    logfile

--- ES/settings.py
+++ ES/settings.py
@@ -124,11 +124,15 @@ if not os.path.exists(LOG_FILE):
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
+
     'formatters': {
         'standard': {
             'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
             'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
         },
     },
     'handlers': {
